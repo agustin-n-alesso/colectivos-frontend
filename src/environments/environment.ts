af3a265efe7ecfb4d1f14d10fa0d81e63be774d7
¨agustin-n-alesso <agustin.alesso@mercadolibre.com>
2023-07-13 19:28:15 -0300

    Ignore environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   server_url: 'http://localhost',
   server_port: 1973,
-  maps_api_key: 'AIzaSyDREfo8k-Hs39bz15wEx-GxRv_zOxq-b-Q'
+  maps_api_key: ''
 };
