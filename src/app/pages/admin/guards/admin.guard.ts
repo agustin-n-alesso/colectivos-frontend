cc48be2b3226bea33d44fa259797412771b83c66
¨agustin-n-alesso <agustin.alesso@mercadolibre.com>
2023-08-31 15:52:57 -0300

    Actualizado Angular

--- src/app/pages/admin/guards/admin.guard.ts
+++ src/app/pages/admin/guards/admin.guard.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AdminGuard implements CanActivate {
+export class AdminGuard  {
 
 	constructor(private router: Router){}
 
